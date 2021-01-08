0407caf1c29d1b138e1582d314212b57e199ee3d
Michael Knoedel <mknoedel@gmail.com>
2021-01-08 15:59:31 -0600

    restructure error code file

--- src/google-cloud/client.ts
+++ src/google-cloud/client.ts
@@ -29,6 +29,54 @@ export type PageableGaxiosResponse<T> = GaxiosResponse<
 
 export type GoogleClientAuth = JWT | Compute | UserRefreshClient;
 
+export async function iterateApi<T>(
+  fn: (nextPageToken?: string) => Promise<PageableGaxiosResponse<T>>,
+  callback: (data: T) => Promise<void>,
+) {
+  let nextPageToken: string | undefined;
+
+  do {
+    const result = await withErrorHandling(fn)(nextPageToken);
+    nextPageToken = result.data.nextPageToken || undefined;
+    await callback(result.data);
+  } while (nextPageToken);
+}
+
+export class Client {
+  readonly projectId: string;
+  readonly iterateApi = iterateApi;
+
+  private credentials: CredentialBody;
+  private auth: GoogleClientAuth;
+
+  constructor({ config }: ClientOptions) {
+    this.projectId = config.serviceAccountKeyConfig.project_id;
+    this.credentials = {
+      client_email: config.serviceAccountKeyConfig.client_email,
+      private_key: config.serviceAccountKeyConfig.private_key,
+    };
+  }
+
+  private async getClient() {
+    const auth = new google.auth.GoogleAuth({
+      credentials: this.credentials,
+      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+    });
+
+    const client = await auth.getClient();
+    await client.getAccessToken();
+    return client;
+  }
+
+  async getAuthenticatedServiceClient(): Promise<GoogleClientAuth> {
+    if (!this.auth) {
+      this.auth = await this.getClient();
+    }
+
+    return this.auth;
+  }
+}
+
 export function withErrorHandling<T extends (...params: any) => any>(fn: T) {
   return async (...params: any) => {
     try {
@@ -77,52 +125,4 @@ function shouldKeepErrorMessage(error: any) {
 
 function createRegex(regexes: string[]) {
   return new RegExp(regexes.map((regex) => '(' + regex + ')').join('|'), 'i');
-}
-
-export async function iterateApi<T>(
-  fn: (nextPageToken?: string) => Promise<PageableGaxiosResponse<T>>,
-  callback: (data: T) => Promise<void>,
-) {
-  let nextPageToken: string | undefined;
-
-  do {
-    const result = await withErrorHandling(fn)(nextPageToken);
-    nextPageToken = result.data.nextPageToken || undefined;
-    await callback(result.data);
-  } while (nextPageToken);
-}
-
-export class Client {
-  readonly projectId: string;
-  readonly iterateApi = iterateApi;
-
-  private credentials: CredentialBody;
-  private auth: GoogleClientAuth;
-
-  constructor({ config }: ClientOptions) {
-    this.projectId = config.serviceAccountKeyConfig.project_id;
-    this.credentials = {
-      client_email: config.serviceAccountKeyConfig.client_email,
-      private_key: config.serviceAccountKeyConfig.private_key,
-    };
-  }
-
-  private async getClient() {
-    const auth = new google.auth.GoogleAuth({
-      credentials: this.credentials,
-      scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
-
-    const client = await auth.getClient();
-    await client.getAccessToken();
-    return client;
-  }
-
-  async getAuthenticatedServiceClient(): Promise<GoogleClientAuth> {
-    if (!this.auth) {
-      this.auth = await this.getClient();
-    }
-
-    return this.auth;
-  }
 }
