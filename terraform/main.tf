c370951a7b07a8b5abff406aace8e5b4d51aaa25
Austin Kelleher <austinlkelleher@gmail.com>
2021-02-17 14:46:55 -0500

    Add required versions to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.57.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
 provider "google" {
   credentials = file(abspath(var.service_account_key_file_path))
   project = var.project_id
