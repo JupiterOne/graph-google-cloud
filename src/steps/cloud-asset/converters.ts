e827b95f9161fb5fd16eca8efd3c6d509534d3ea
Austin Kelleher <austin.kelleher@jupiterone.com>
2021-06-09 16:24:40 -0400

    Fixes #237 - Prevent duplicate google_iam_binding _key values

--- src/steps/cloud-asset/converters.ts
+++ src/steps/cloud-asset/converters.ts
@@ -4,18 +4,37 @@ import { snakeCase } from 'lodash';
 import { createGoogleCloudIntegrationEntity } from '../../utils/entity';
 import { bindingEntities } from './constants';
 
-export function createIamBindingEntity(
-  binding: cloudasset_v1.Schema$Binding,
-  project: string | undefined | null,
-  resource: string | undefined | null,
-) {
-  // There are no unique values returned from this result so use everything available to make the binding key unique.
+export function buildIamBindingEntityKey({
+  binding,
+  project,
+  resource,
+}: {
+  binding: cloudasset_v1.Schema$Binding;
+  project: string | undefined | null;
+  resource: string | undefined | null;
+}) {
+  // There are no unique values returned from this result so use everything
+  // available to make the binding key unique.
   const keyBuilders = ['binding'];
+
   if (project) keyBuilders.push('project:' + project);
   if (resource) keyBuilders.push('resource:' + resource);
   if (binding.role) keyBuilders.push('role:' + binding.role);
-  const key = keyBuilders.join('|');
 
+  return keyBuilders.join('|');
+}
+
+export function createIamBindingEntity({
+  _key,
+  binding,
+  project,
+  resource,
+}: {
+  _key: string;
+  binding: cloudasset_v1.Schema$Binding;
+  project: string | undefined | null;
+  resource: string | undefined | null;
+}) {
   const namePrefix = 'Role Binding for Resource: ';
 
   return createGoogleCloudIntegrationEntity(binding, {
@@ -24,7 +43,7 @@ export function createIamBindingEntity(
       assign: {
         _class: bindingEntities.BINDINGS._class,
         _type: bindingEntities.BINDINGS._type,
-        _key: key,
+        _key,
         name: snakeCase(namePrefix) + resource,
         displayName: namePrefix + resource,
         resource,
