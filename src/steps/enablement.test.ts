a1860de638c476724b92053539e6fa715aefb111
Austin Kelleher <austin.kelleher@jupiterone.com>
2021-06-22 18:15:20 -0400

    Revert fix for #248

--- src/steps/enablement.test.ts
+++ src/steps/enablement.test.ts
@@ -3,6 +3,25 @@ import { ServiceUsageName } from '../google-cloud/types';
 import * as enablement from './enablement';
 import * as serviceUsage from './service-usage/client';
 
+describe('#getUniqueIntegrationConfigProjectsForStepEnablement', () => {
+  test('should return only "main" integration project ID if no "target" project ID is supplied', () => {
+    const config = getMockIntegrationConfig();
+    expect(
+      enablement.getUniqueIntegrationConfigProjectsForStepEnablement(config),
+    ).toEqual(['j1-gc-integration-dev-v2']);
+  });
+
+  test('should return both "main" and "target" integration project IDs if "target" project ID is supplied', () => {
+    const config = getMockIntegrationConfig({
+      projectId: 'my-target-project-id',
+    });
+
+    expect(
+      enablement.getUniqueIntegrationConfigProjectsForStepEnablement(config),
+    ).toEqual(['j1-gc-integration-dev-v2', 'my-target-project-id']);
+  });
+});
+
 describe('#createStepStartState', () => {
   test('should not mark step start state as disabled when the service is enabled', () => {
     expect(
@@ -94,5 +113,32 @@ describe('#getEnabledServiceNames', () => {
       mockEnabledServiceNames,
     );
     expect(collectEnabledServicesForProjectSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test('should return enabled service names intersection between "main" and "target" projects if "target" project specified in config', async () => {
+    const mockConfig = getMockIntegrationConfig({
+      projectId: 'my-target-project-id',
+    });
+
+    const mainMockEnabledServiceNames: string[] = [
+      'pubsub.googleapis.com',
+      'appengine.googleapis.com',
+      'dns.googleapis.com',
+    ];
+
+    const targetMockEnabledServiceNames: string[] = [
+      'appengine.googleapis.com',
+    ];
+
+    const collectEnabledServicesForProjectSpy = jest
+      .spyOn(serviceUsage, 'collectEnabledServicesForProject')
+      .mockResolvedValue(Promise.resolve(mainMockEnabledServiceNames))
+      .mockResolvedValue(Promise.resolve(targetMockEnabledServiceNames));
+
+    expect(await enablement.getEnabledServiceNames(mockConfig)).toEqual([
+      'appengine.googleapis.com',
+    ]);
+
+    expect(collectEnabledServicesForProjectSpy).toHaveBeenCalledTimes(2);
   });
 });
