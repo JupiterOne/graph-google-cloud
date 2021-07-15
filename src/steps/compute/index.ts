cadf369a990dc8b19921fe0efc8769dc5b528cfa
Stefan Virag <viragsf@gmail.com>
2021-07-16 01:38:27 +0200

    Handle deleted custom images

--- src/steps/compute/index.ts
+++ src/steps/compute/index.ts
@@ -429,6 +429,31 @@ export async function fetchComputeDisks(
               to: imageEntity,
             }),
           );
+        } else {
+          // The custom image no longer exist most likely because it got deleted
+          const placeholderImageEntity = await jobState.addEntity(
+            createComputeImageEntity({
+              data: {
+                // We need an unique id for the key
+                id: `${sourceImageProjectId}:${sourceImageName}:deleted`,
+                name: sourceImageName,
+                // Converter already knows how to handle this
+                deprecated: {
+                  state: 'DELETED',
+                },
+              } as compute_v1.Schema$Image,
+              // It doesn't exist anymore
+              isPublic: false,
+            }),
+          );
+
+          await jobState.addRelationship(
+            createDirectRelationship({
+              _class: RelationshipClass.USES,
+              from: diskEntity,
+              to: placeholderImageEntity,
+            }),
+          );
         }
       } else {
         // Public image case
