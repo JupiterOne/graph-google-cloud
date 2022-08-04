4cfe76ecd1f29009488ac9f08ad38d1dc321e1ce
Ignacio Rodriguez <ignacio@fosfori.to>
2022-08-04 12:24:37 -0300

    converter skeleton

--- src/steps/secret-manager/converters.ts
+++ src/steps/secret-manager/converters.ts
@@ -1,24 +1,19 @@
-import { cloudbilling_v1 } from 'googleapis';
+import { secretmanager_v1beta1 } from 'googleapis';
 import { createGoogleCloudIntegrationEntity } from '../../utils/entity';
 import {
-  ENTITY_CLASS_BILLING_ACCOUNT,
-  ENTITY_TYPE_BILLING_ACCOUNT,
+  ENTITY_CLASS_SECRET_MANAGER_SECRET,
+  ENTITY_TYPE_SECRET_MANAGER_SECRET,
 } from './constants';
 
-export function createBillingAccountEntity(
-  data: cloudbilling_v1.Schema$BillingAccount,
-) {
+export function createSecretEntitiy(data: secretmanager_v1beta1.Schema$Secret) {
   return createGoogleCloudIntegrationEntity(data, {
     entityData: {
       source: data,
       assign: {
-        _class: ENTITY_CLASS_BILLING_ACCOUNT,
-        _type: ENTITY_TYPE_BILLING_ACCOUNT,
+        _class: ENTITY_CLASS_SECRET_MANAGER_SECRET,
+        _type: ENTITY_TYPE_SECRET_MANAGER_SECRET,
         _key: data.name as string,
         name: data.name,
-        displayName: data.displayName as string,
-        open: data.open,
-        masterBillingAccount: data.masterBillingAccount,
       },
     },
   });
