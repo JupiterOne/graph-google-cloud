5f36ed27c0ee09a56732b15fa3207cb99d7e3729
Ignacio Rodriguez <ignacio@fosfori.to>
2022-08-24 00:30:22 -0300

    Updated tests for cloud-build

--- src/steps/cloud-build/converters.ts
+++ src/steps/cloud-build/converters.ts
@@ -1,3 +1,4 @@
+import { parseTimePropertyValue } from '@jupiterone/integration-sdk-core';
 import { cloudbuild_v1 } from 'googleapis';
 import { createGoogleCloudIntegrationEntity } from '../../utils/entity';
 import { CloudBuildEntities } from './constants';
@@ -17,6 +18,24 @@ export function createBuildEntity(source: cloudbuild_v1.Schema$Build) {
         _type: CloudBuildEntities.BUILD._type,
         _key: data.name as string,
         name: data.name,
+        id: data.id as string,
+        status: data.status,
+        'source.storageSource.bucket': data.source?.storageSource?.bucket,
+        'source.storageSource.object': data.source?.storageSource?.object,
+        'source.storageSource.generation':
+          data.source?.storageSource?.generation,
+        'results.buildStepImages': data.results?.buildStepImages,
+        'results.buildStepOutputs': data.results?.buildStepOutputs,
+        createTime: parseTimePropertyValue(data.createTime),
+        startTime: parseTimePropertyValue(data.startTime),
+        finishTime: parseTimePropertyValue(data.finishTime),
+        timeout: data.timeout,
+        projectId: data.projectId,
+        logsBucket: data.logsBucket,
+        'options.logging': data.options?.logging,
+        'options.pool': data.options?.workerPool,
+        logUrl: data.logUrl,
+        queueTtl: data.queueTtl,
       },
     },
   });
