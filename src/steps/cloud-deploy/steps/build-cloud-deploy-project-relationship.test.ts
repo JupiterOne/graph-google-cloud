2cb1939bd00bf95e46d71df9b088a373af775405
bharatkk-metron <bharat.kumavat@metronlabs.com>
2024-04-11 13:55:10 +0530

    project has cloud deploy

--- src/steps/cloud-deploy/steps/build-cloud-deploy-project-relationship.test.ts
+++ src/steps/cloud-deploy/steps/build-cloud-deploy-project-relationship.test.ts
@@ -1,40 +1,147 @@
 import {
-  executeStepWithDependencies,
+  createMockStepExecutionContext,
+  filterGraphObjects,
   Recording,
-  StepTestConfig,
 } from '@jupiterone/integration-sdk-testing';
-import { invocationConfig } from '../../..';
+import { IntegrationConfig } from '../../..';
 import { integrationConfig } from '../../../../test/config';
+import { setupGoogleCloudRecording } from '../../../../test/recording';
 import {
-  getMatchRequestsBy,
-  setupGoogleCloudRecording,
-} from '../../../../test/recording';
-import { STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP } from '../constant';
+  ENTITY_CLASS_CLOUD_DEPLOY_SERVICE,
+  ENTITY_TYPE_CLOUD_DEPLOY_SERVICE,
+  RELATIONSHIP_TYPE_PROJECT_HAS_CLOUD_DEPLOY,
+  STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP,
+} from '../constant';
+import {
+  ExplicitRelationship,
+  Relationship,
+} from '@jupiterone/integration-sdk-core';
+import { fetchCloudDeploySevice } from './cloud-deploy-service';
+import {
+  fetchResourceManagerProject,
+  PROJECT_ENTITY_TYPE,
+} from '../../resource-manager';
+import { buildProjectCloudDeployServiceRelationship } from './build-cloud-deploy-project-relationship';
 
-describe(STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP, () => {
+describe('Build Project and Cloud Deploy Service Relationship', () => {
   let recording: Recording;
-  afterEach(async () => {
-    if (recording) await recording.stop();
+
+  beforeEach(() => {
+    recording = setupGoogleCloudRecording({
+      directory: __dirname,
+      name: STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP,
+    });
   });
 
-  jest.setTimeout(450000);
+  afterEach(async () => {
+    await recording.stop();
+  });
 
-  test.skip(STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP, async () => {
-    recording = setupGoogleCloudRecording({
-      name: STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP,
-      directory: __dirname,
-      options: {
-        matchRequestsBy: getMatchRequestsBy(integrationConfig),
+  function separateRelationships(collectedRelationships: Relationship[]) {
+    const { targets: directRelationships } = filterGraphObjects(
+      collectedRelationships,
+      (r) => !r._mapping,
+    ) as {
+      targets: ExplicitRelationship[];
+    };
+    return {
+      directRelationships,
+    };
+  }
+
+  test('should collect data', async () => {
+    const context = createMockStepExecutionContext<IntegrationConfig>({
+      instanceConfig: {
+        ...integrationConfig,
+        serviceAccountKeyFile: integrationConfig.serviceAccountKeyFile.replace(
+          'j1-gc-integration-dev-v2',
+          'j1-gc-integration-dev-v3',
+        ),
+        serviceAccountKeyConfig: {
+          ...integrationConfig.serviceAccountKeyConfig,
+          project_id: 'j1-gc-integration-dev-v3',
+        },
       },
     });
 
-    const stepTestConfig: StepTestConfig = {
-      stepId: STEP_PROJECT_HAS_CLOUD_DEPLOY_RELATIONSHIP,
-      instanceConfig: integrationConfig,
-      invocationConfig: invocationConfig as any,
-    };
+    await fetchCloudDeploySevice(context);
+    await fetchResourceManagerProject(context);
+    await buildProjectCloudDeployServiceRelationship(context);
 
-    const result = await executeStepWithDependencies(stepTestConfig);
-    expect(result).toMatchStepMetadata(stepTestConfig);
+    expect({
+      numCollectedEntities: context.jobState.collectedEntities.length,
+      numCollectedRelationships: context.jobState.collectedRelationships.length,
+      collectedEntities: context.jobState.collectedEntities,
+      collectedRelationships: context.jobState.collectedRelationships,
+      encounteredTypes: context.jobState.encounteredTypes,
+    }).toMatchSnapshot();
+
+    expect(
+      context.jobState.collectedEntities.filter(
+        (e) => e._type === PROJECT_ENTITY_TYPE,
+      ),
+    ).toMatchGraphObjectSchema({
+      _class: ['Account'],
+      schema: {
+        additionalProperties: false,
+        properties: {
+          _type: { const: 'google_cloud_project' },
+          _rawData: {
+            type: 'array',
+            items: { type: 'object' },
+          },
+          projectId: { type: 'string' },
+          name: { type: 'string' },
+          displayName: { type: 'string' },
+          parent: { type: 'string' },
+          lifecycleState: { type: 'string' },
+          createdOn: { type: 'number' },
+          updatedOn: { type: 'number' },
+        },
+      },
+    });
+
+    expect(
+      context.jobState.collectedEntities.filter(
+        (e) => e._type === ENTITY_TYPE_CLOUD_DEPLOY_SERVICE,
+      ),
+    ).toMatchGraphObjectSchema({
+      _class: ENTITY_CLASS_CLOUD_DEPLOY_SERVICE,
+      schema: {
+        additionalProperties: false,
+        properties: {
+          _type: { const: ENTITY_TYPE_CLOUD_DEPLOY_SERVICE },
+          _rawData: {
+            type: 'array',
+            items: { type: 'object' },
+          },
+          category: { type: 'array', items: { type: 'string' } },
+          function: { type: 'array', items: { type: 'string' } },
+          name: { type: 'string' },
+          projectId: { type: 'string' },
+          organizationId: { type: 'string' },
+          instanceId: { type: 'string' },
+        },
+      },
+    });
+
+    const { directRelationships } = separateRelationships(
+      context.jobState.collectedRelationships,
+    );
+
+    expect(
+      directRelationships.filter(
+        (e) => e._type === RELATIONSHIP_TYPE_PROJECT_HAS_CLOUD_DEPLOY,
+      ),
+    ).toMatchDirectRelationshipSchema({
+      schema: {
+        properties: {
+          _class: { const: 'HAS' },
+          _type: {
+            const: RELATIONSHIP_TYPE_PROJECT_HAS_CLOUD_DEPLOY,
+          },
+        },
+      },
+    });
   });
 });
