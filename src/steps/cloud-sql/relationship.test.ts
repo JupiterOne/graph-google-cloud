9ff960cb09173d18f162a7052a4ae4115b305649
bharatkk-metron <bharat.kumavat@metronlabs.com>
2024-06-03 09:42:09 +0530

    unit tests corrected

--- src/steps/cloud-sql/relationship.test.ts
+++ src/steps/cloud-sql/relationship.test.ts
@@ -13,6 +13,7 @@ import {
 } from '../../../test/recording';
 import {
   ENTITY_CLASS_CLOUD_SQL,
+  ENTITY_CLASS_CLOUD_SQL_INSTANCES,
   ENTITY_TYPE_CLOUD_SQL,
   ENTITY_TYPE_CLOUD_SQL_INSTANCES,
   RELATIONSHIP_TYPE_CLOUD_PROJECT_HAS_CLOUD_SQL,
@@ -26,9 +27,20 @@ import {
   STEP_CLOUD_SQL_INSTANCES_USES_CLOUD_SQL_SSL_CERTIFICATION,
   STEP_GOOGLE_CLOUD_PROJECT_HAS_CLOUD_SQL,
 } from './constants';
-import { ExplicitRelationship, Relationship } from '@jupiterone/integration-sdk-core';
-import { buildProjectHasSqlRelationship, buildSqlHasSqlInstancesRelationship, fetchCloudSql, fetchCloudSqlInstances } from '.';
-import { fetchResourceManagerProject, PROJECT_ENTITY_TYPE } from '../resource-manager';
+import {
+  ExplicitRelationship,
+  Relationship,
+} from '@jupiterone/integration-sdk-core';
+import {
+  buildProjectHasSqlRelationship,
+  buildSqlHasSqlInstancesRelationship,
+  fetchCloudSql,
+  fetchCloudSqlInstances,
+} from '.';
+import {
+  fetchResourceManagerProject,
+  PROJECT_ENTITY_TYPE,
+} from '../resource-manager';
 
 describe(`Cloud-Sql#${STEP_GOOGLE_CLOUD_PROJECT_HAS_CLOUD_SQL}`, () => {
   let recording: Recording;
@@ -52,96 +64,99 @@ describe(`Cloud-Sql#${STEP_GOOGLE_CLOUD_PROJECT_HAS_CLOUD_SQL}`, () => {
       directRelationships,
     };
   }
-  test(STEP_GOOGLE_CLOUD_PROJECT_HAS_CLOUD_SQL, async () => {
-    const context = createMockStepExecutionContext<IntegrationConfig>({
-      instanceConfig: {
-        ...integrationConfig,
-        serviceAccountKeyFile: integrationConfig.serviceAccountKeyFile.replace(
-          'j1-gc-integration-dev-v2',
-          'j1-gc-integration-dev-v3',
+  test(
+    STEP_GOOGLE_CLOUD_PROJECT_HAS_CLOUD_SQL,
+    async () => {
+      const context = createMockStepExecutionContext<IntegrationConfig>({
+        instanceConfig: {
+          ...integrationConfig,
+          serviceAccountKeyFile:
+            integrationConfig.serviceAccountKeyFile.replace(
+              'j1-gc-integration-dev-v2',
+              'j1-gc-integration-dev-v3',
+            ),
+          serviceAccountKeyConfig: {
+            ...integrationConfig.serviceAccountKeyConfig,
+            project_id: 'j1-gc-integration-dev-v3',
+          },
+        },
+      });
+      await fetchCloudSql(context);
+      await fetchResourceManagerProject(context);
+      await buildProjectHasSqlRelationship(context);
+      expect({
+        numCollectedEntities: context.jobState.collectedEntities.length,
+        numCollectedRelationships:
+          context.jobState.collectedRelationships.length,
+        collectedEntities: context.jobState.collectedEntities,
+        collectedRelationships: context.jobState.collectedRelationships,
+        encounteredTypes: context.jobState.encounteredTypes,
+      }).toMatchSnapshot();
+      expect(
+        context.jobState.collectedEntities.filter(
+          (e) => e._type === PROJECT_ENTITY_TYPE,
         ),
-        serviceAccountKeyConfig: {
-          ...integrationConfig.serviceAccountKeyConfig,
-          project_id: 'j1-gc-integration-dev-v3',
-        },
-      },
-    });
-    await fetchCloudSql(context);
-    await fetchResourceManagerProject(context);
-    await buildProjectHasSqlRelationship(context);
-    expect({
-      numCollectedEntities: context.jobState.collectedEntities.length,
-      numCollectedRelationships: context.jobState.collectedRelationships.length,
-      collectedEntities: context.jobState.collectedEntities,
-      collectedRelationships: context.jobState.collectedRelationships,
-      encounteredTypes: context.jobState.encounteredTypes,
-    }).toMatchSnapshot();
-    expect(
-      context.jobState.collectedEntities.filter(
-        (e) => e._type === PROJECT_ENTITY_TYPE,
-      ),
-    ).toMatchGraphObjectSchema({
-      _class: ['Account'],
-      schema: {
-        additionalProperties: false,
-        properties: {
-          _type: { const: 'google_cloud_project' },
-          _rawData: {
-            type: 'array',
-            items: { type: 'object' },
-          },
-          projectId: { type: 'string' },
-          name: { type: 'string' },
-          displayName: { type: 'string' },
-          parent: { type: 'string' },
-          lifecycleState: { type: 'string' },
-          createdOn: { type: 'number' },
-          updatedOn: { type: 'number' },
-        },
-      },
-    });
-    expect(
-      context.jobState.collectedEntities.filter(
-        (e) => e._type === ENTITY_TYPE_CLOUD_SQL,
-      ),
-    ).toMatchGraphObjectSchema({
-      _class: ENTITY_CLASS_CLOUD_SQL,
-      schema: {
-        additionalProperties: false,
-        properties: {
-          _type: { const: ENTITY_TYPE_CLOUD_SQL },
-          _rawData: {
-            type: 'array',
-            items: { type: 'object' },
-          },
-          name: { type: 'string' },
-          function: { type: 'array' },
-          category: { type: 'array' },
-        },
-      },
-    });
-    const { directRelationships } = separateRelationships(
-      context.jobState.collectedRelationships,
-    );
-    expect(
-      directRelationships.filter(
-        (e) =>
-          e._type ===
-          RELATIONSHIP_TYPE_CLOUD_PROJECT_HAS_CLOUD_SQL,
-      ),
-    ).toMatchDirectRelationshipSchema({
-      schema: {
-        properties: {
-          _class: { const: 'HAS' },
-          _type: {
-            const: RELATIONSHIP_TYPE_CLOUD_PROJECT_HAS_CLOUD_SQL,
+      ).toMatchGraphObjectSchema({
+        _class: ['Account'],
+        schema: {
+          additionalProperties: false,
+          properties: {
+            _type: { const: 'google_cloud_project' },
+            _rawData: {
+              type: 'array',
+              items: { type: 'object' },
+            },
+            projectId: { type: 'string' },
+            name: { type: 'string' },
+            displayName: { type: 'string' },
+            parent: { type: 'string' },
+            lifecycleState: { type: 'string' },
+            createdOn: { type: 'number' },
+            updatedOn: { type: 'number' },
           },
         },
-      },
-    });
-  }, 100000);
+      });
+      expect(
+        context.jobState.collectedEntities.filter(
+          (e) => e._type === ENTITY_TYPE_CLOUD_SQL,
+        ),
+      ).toMatchGraphObjectSchema({
+        _class: ENTITY_CLASS_CLOUD_SQL,
+        schema: {
+          additionalProperties: false,
+          properties: {
+            _type: { const: ENTITY_TYPE_CLOUD_SQL },
+            _rawData: {
+              type: 'array',
+              items: { type: 'object' },
+            },
+            name: { type: 'string' },
+            function: { type: 'array' },
+            category: { type: 'array' },
+          },
+        },
+      });
+      const { directRelationships } = separateRelationships(
+        context.jobState.collectedRelationships,
+      );
+      expect(
+        directRelationships.filter(
+          (e) => e._type === RELATIONSHIP_TYPE_CLOUD_PROJECT_HAS_CLOUD_SQL,
+        ),
+      ).toMatchDirectRelationshipSchema({
+        schema: {
+          properties: {
+            _class: { const: 'HAS' },
+            _type: {
+              const: RELATIONSHIP_TYPE_CLOUD_PROJECT_HAS_CLOUD_SQL,
+            },
+          },
+        },
+      });
+    },
+    100000,
+  );
 });
-
 
 describe(`Cloud-Sql#${STEP_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES}`, () => {
   let recording: Recording;
@@ -165,94 +180,96 @@ describe(`Cloud-Sql#${STEP_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES}`, () => {
       directRelationships,
     };
   }
-  test(STEP_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES, async () => {
-    const context = createMockStepExecutionContext<IntegrationConfig>({
-      instanceConfig: {
-        ...integrationConfig,
-        serviceAccountKeyFile: integrationConfig.serviceAccountKeyFile.replace(
-          'j1-gc-integration-dev-v2',
-          'j1-gc-integration-dev-v3',
+  test(
+    STEP_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES,
+    async () => {
+      const context = createMockStepExecutionContext<IntegrationConfig>({
+        instanceConfig: {
+          ...integrationConfig,
+          serviceAccountKeyFile:
+            integrationConfig.serviceAccountKeyFile.replace(
+              'j1-gc-integration-dev-v2',
+              'j1-gc-integration-dev-v3',
+            ),
+          serviceAccountKeyConfig: {
+            ...integrationConfig.serviceAccountKeyConfig,
+            project_id: 'j1-gc-integration-dev-v3',
+          },
+        },
+      });
+      await fetchCloudSql(context);
+      await fetchCloudSqlInstances(context);
+      await buildSqlHasSqlInstancesRelationship(context);
+      expect({
+        numCollectedEntities: context.jobState.collectedEntities.length,
+        numCollectedRelationships:
+          context.jobState.collectedRelationships.length,
+        collectedEntities: context.jobState.collectedEntities,
+        collectedRelationships: context.jobState.collectedRelationships,
+        encounteredTypes: context.jobState.encounteredTypes,
+      }).toMatchSnapshot();
+      expect(
+        context.jobState.collectedEntities.filter(
+          (e) => e._type === ENTITY_TYPE_CLOUD_SQL,
         ),
-        serviceAccountKeyConfig: {
-          ...integrationConfig.serviceAccountKeyConfig,
-          project_id: 'j1-gc-integration-dev-v3',
-        },
-      },
-    });
-    await fetchCloudSql(context);
-    await fetchCloudSqlInstances(context);
-    await buildSqlHasSqlInstancesRelationship(context);
-    expect({
-      numCollectedEntities: context.jobState.collectedEntities.length,
-      numCollectedRelationships: context.jobState.collectedRelationships.length,
-      collectedEntities: context.jobState.collectedEntities,
-      collectedRelationships: context.jobState.collectedRelationships,
-      encounteredTypes: context.jobState.encounteredTypes,
-    }).toMatchSnapshot();
-    expect(
-      context.jobState.collectedEntities.filter(
-        (e) => e._type === ENTITY_TYPE_CLOUD_SQL,
-      ),
-    ).toMatchGraphObjectSchema({
-      _class: ENTITY_CLASS_CLOUD_SQL,
-      schema: {
-        additionalProperties: false,
-        properties: {
-          _type: { const: ENTITY_TYPE_CLOUD_SQL },
-          _rawData: {
-            type: 'array',
-            items: { type: 'object' },
-          },
-          name: { type: 'string' },
-          function: { type: 'array' },
-          category: { type: 'array' },
-        },
-      },
-    });
-    expect(
-      context.jobState.collectedEntities.filter(
-        (e) => e._type === ENTITY_TYPE_CLOUD_SQL_INSTANCES,
-      ),
-    ).toMatchGraphObjectSchema({
-      _class: ENTITY_CLASS_CLOUD_SQL,
-      schema: {
-        additionalProperties: false,
-        properties: {
-          _type: { const: ENTITY_TYPE_CLOUD_SQL_INSTANCES },
-          _rawData: {
-            type: 'array',
-            items: { type: 'object' },
-          },
-          name: { type: 'string' },
-          etag: { type: 'string' },
-          url: { type: 'string' },
-          projectId: { type: 'string' },
-          kind: { type: 'string' },
-          databaseInstalledVersion: { type: 'string' },
-          connectionName: { type: 'string' },
-        },
-      },
-    });
-    const { directRelationships } = separateRelationships(
-      context.jobState.collectedRelationships,
-    );
-    expect(
-      directRelationships.filter(
-        (e) =>
-          e._type ===
-        RELATIONSHIP_TYPE_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES,
-      ),
-    ).toMatchDirectRelationshipSchema({
-      schema: {
-        properties: {
-          _class: { const: 'HAS' },
-          _type: {
-            const: RELATIONSHIP_TYPE_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES,
+      ).toMatchGraphObjectSchema({
+        _class: ENTITY_CLASS_CLOUD_SQL,
+        schema: {
+          additionalProperties: false,
+          properties: {
+            _type: { const: ENTITY_TYPE_CLOUD_SQL },
+            _rawData: {
+              type: 'array',
+              items: { type: 'object' },
+            },
+            name: { type: 'string' },
+            function: { type: 'array' },
+            category: { type: 'array' },
           },
         },
-      },
-    });
-  }, 100000);
+      });
+      expect(
+        context.jobState.collectedEntities.filter(
+          (e) => e._type === ENTITY_TYPE_CLOUD_SQL_INSTANCES,
+        ),
+      ).toMatchGraphObjectSchema({
+        _class: ENTITY_CLASS_CLOUD_SQL_INSTANCES,
+        schema: {
+          additionalProperties: false,
+          properties: {
+            _type: { const: ENTITY_TYPE_CLOUD_SQL_INSTANCES },
+            _rawData: {},
+            name: { type: 'string' },
+            etag: { type: 'string' },
+            url: { type: 'string' },
+            projectId: { type: 'string' },
+            kind: { type: 'string' },
+            databaseInstalledVersion: { type: 'string' },
+            connectionName: { type: 'string' },
+          },
+        },
+      });
+      const { directRelationships } = separateRelationships(
+        context.jobState.collectedRelationships,
+      );
+      expect(
+        directRelationships.filter(
+          (e) =>
+            e._type === RELATIONSHIP_TYPE_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES,
+        ),
+      ).toMatchDirectRelationshipSchema({
+        schema: {
+          properties: {
+            _class: { const: 'HAS' },
+            _type: {
+              const: RELATIONSHIP_TYPE_CLOUD_SQL_HAS_CLOUD_SQL_INSTANCES,
+            },
+          },
+        },
+      });
+    },
+    100000,
+  );
 });
 
 describe(`Cloud-Sql#${STEP_CLOUD_SQL_INSTANCES_ASSIGNED_GOOGLE_USER}`, () => {
